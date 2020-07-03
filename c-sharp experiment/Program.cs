75c1f575ce34095d99e61a59176a428df892ce0d
W <woldsom@users.noreply.github.com>
2020-07-03 14:29:08 +0200

    Template generated file

---
+++ c-sharp experiment/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace c_sharp_experiment
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
